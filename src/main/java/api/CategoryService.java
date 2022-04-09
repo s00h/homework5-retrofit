7f95732ea4723b0d25de836d700c648e8f6e4325
s00h <kuzmin@dr.com>
2022-04-09 20:30:30 +0300

    commit

---
+++ src/main/java/api/CategoryService.java
@@ -0,0 +1,11 @@
+package api;
+
+import dto.GetCategoryResponse;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface CategoryService {
+    @GET("categories/{id}")
+    Call<GetCategoryResponse> getCategory(@Path("id") int id);
+}
